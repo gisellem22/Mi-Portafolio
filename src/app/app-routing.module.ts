898b5d5811cf217120801072e35b0e0b7193294a
Giselle Machado <gisellemachadoyes@gmail.com>
2019-08-18 20:39:37 -0400

    se actualiza navbar en app.component. Responsive con Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
   imports: [    
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: false } // <-- debugging purposes only
+      { enableTracing: false }
     )
   ],
   exports: [RouterModule]
