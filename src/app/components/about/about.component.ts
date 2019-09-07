4eabe05195bd1787ed5a5663239f28d667a6973f
Giselle Machado <gisellemachadoyes@gmail.com>
2019-09-07 17:57:05 -0400

    removiendo el componente skill y agregando sus info al component about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillsService } from '../../services/skills.service'
+import { skill } from 'src/app/models/skill';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  skills: skill[];
 
-  constructor() { }
+  constructor(public SkillsService: SkillsService) { }
 
   ngOnInit() {
+    this.SkillsService.getSkillsList().subscribe(skills => {
+      this.skills =skills;
+      console.log(this.skills)
+    });
   }
-
 }
--- src/app/projects-list.ts
+++ src/app/projects-list.ts
@@ -2,16 +2,10 @@ import { project } from './models/project';
 
 export let projects: project[] = [
     {
-        name: "POKEMÓN - I CHOOSE YOU",
-        text: "Enciclopedia que agrupa información de los Pokémons de la primera generación, permitiendo al usuario realizar diversos filtros. Para su desarrollo se hizo consumo de una Api Rest de Pokémon, JavaScript y Bootstrap.",
-        img: "../../../assets/img/projects/pokemon.JPG",
-        demo: "https://gisellem22.github.io/SCL009-data-lovers/src/index.html",
-        code: "https://github.com/gisellem22/SCL009-data-lovers"
-    },
-    {
         name: "GM-MD-LINKS",
         text: "Librería NPM para extraer los enlaces de directorios y/o archivos markdown (.md), validar sus status y obtener estadísticas. Desarrollada con JavaScript y Node Js.",
         img: "../../../assets/img/projects/gm-md-links.JPG",
+        // demo: "https://www.npmjs.com/package/gm-md-links",
         code: "https://github.com/gisellem22/SCL009-md-links"
     },
     {
@@ -22,8 +16,15 @@ export let projects: project[] = [
         code: "https://github.com/gisellem22/SCL009-Otras-Miradas"
     },
     {
+        name: "POKEMÓN - I CHOOSE YOU",
+        text: "Enciclopedia que agrupa información de los Pokémons de la primera generación, permitiendo al usuario realizar diversos filtros. Para su desarrollo se hizo consumo de una Api Rest de Pokémon, JavaScript y Bootstrap.",
+        img: "../../../assets/img/projects/pokemon.JPG",
+        demo: "https://gisellem22.github.io/SCL009-data-lovers/src/index.html",
+        code: "https://github.com/gisellem22/SCL009-data-lovers"
+    },
+    {
         name: "BURGUER QUEEN",
-        text: "Plataforma web SPA diseñada para Tablet, que permite a los meseros de una cadena de restaurantes tomar los pedidos de los clientes.Implementado con Angular y Bootstrap como frameworks Front-End. FireStore como base de datos RealTime para almacenar los pedidos realizados.",
+        text: "Plataforma web SPA diseñada para Tablet, que permite a los meseros de una cadena de restaurantes tomar los pedidos de los clientes. Implementado con Angular y Bootstrap como frameworks Front-End. FireStore como base de datos RealTime para almacenar los pedidos realizados.",
         img: "../../../assets/img/projects/burger-queen.JPG",
         demo: "https://burger-queen-dg.firebaseapp.com",
         code: "https://github.com/gisellem22/SCL009-Burger-Queen"
@@ -35,7 +36,7 @@ export let projects: project[] = [
         code: "https://github.com/gisellem22/Deseafio-Hotel"
     },
     {
-        name: "Gigibook",
+        name: "GIGIBOOK",
         text: "Interfaz web que represente la sección de comentarios de una plataforma. Implementada con Angular, Firebase RealTime Firestore y Bootstrap.",
         img: "../../../assets/img/projects/comentarios.JPG",
         demo: "https://desafio-comentarios.firebaseapp.com",
