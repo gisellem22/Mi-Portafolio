a3a36491f694b260763e461a08acabb3efba2fe4
Giselle Machado <gisellemachadoyes@gmail.com>
2020-01-14 22:15:59 -0300

    se acatualza foto de proyecto modyo

--- src/app/projects-list.ts
+++ src/app/projects-list.ts
@@ -9,34 +9,26 @@ export let projects: project[] = [
         code: "https://github.com/gisellem22/SCL009-md-links"
     },
     {
-        name: "OTRAS MIRADAS",
-        text: "Plataforma web SPA con diseño mobile first desarrollada para servir de apoyo a los padres de niños con necesidades especiales de La Fundación Otras Miradas. Proyecto implementado con Angular, Bootstrap y Firebase.",
-        img: "../../../assets/img/projects/otras-miradas.png",
-        demo: "https://otrasmiradas01.firebaseapp.com/home",
-        code: "https://github.com/gisellem22/SCL009-Otras-Miradas"
+        name: "LANDERZ.",
+        text: "Landing page responsiva implementada en HTML, CSS y Javascript, consumo de API Rest y librerías Bootstrap, Slick y Axios.",
+        img: "../../../assets/img/projects/modyo.png",
+        demo: "https://gisellem22.github.io/Modyo-Project/src/index.html",
+        code: "https://github.com/gisellem22/Modyo-Project"
     },
+    // {
+    //     name: "OTRAS MIRADAS",
+    //     text: "Plataforma web SPA con diseño mobile first desarrollada para servir de apoyo a los padres de niños con necesidades especiales de La Fundación Otras Miradas. Proyecto implementado con Angular, Bootstrap y Firebase.",
+    //     img: "../../../assets/img/projects/otras-miradas.png",
+    //     demo: "https://otrasmiradas01.firebaseapp.com/home",
+    //     code: "https://github.com/gisellem22/SCL009-Otras-Miradas"
+    // },
     {
-        name: "WOOD HOTEL-GROUP",
-        text: "Interfaz web que ayude a un cliente en línea a encontrar el hotel más barato. Implementada con Angular, Firebase RealTime Firestore y Bootstrap.",
-        img: "../../../assets/img/projects/hotel.JPG",
-        demo: "https://desafio-hotel.firebaseapp.com",
-        code: "https://github.com/gisellem22/Deseafio-Hotel"
-    },
-    {
-        name: "Github Search",
-        text: "Aplicación SPA que consume la API de Github y ve los repositorios de un usuario específico. Desarrollado en Angular, despliegue en Firebase. CSS responsivo sin uso de frameworks.",
+        name: "GITHUB SEARCH",
+        text: "Aplicación SPA que consume APIS de Github y ve los repositorios de un usuario específico. Desarrollado en Angular, despliegue en Firebase. CSS responsivo sin uso de frameworks.",
         img: "../../../assets/img/projects/github.png",
         demo: "https://concrete-challenge.web.app/home",
         code: "https://github.com/gisellem22/frontend-recruiting-hsa"
     },
-    // },
-    // {
-    //     name: "GIGIBOOK",
-    //     text: "Interfaz web que represente la sección de comentarios de una plataforma. Implementada con Angular, Firebase RealTime Firestore y Bootstrap.",
-    //     img: "../../../assets/img/projects/comentarios.JPG",
-    //     demo: "https://desafio-comentarios.firebaseapp.com",
-    //     code: "https://github.com/gisellem22/Desafio-Comentarios"
-    // },
     {
         name: "POKEMÓN - I CHOOSE YOU",
         text: "Enciclopedia que agrupa información de los Pokémons de la primera generación, permitiendo al usuario realizar diversos filtros. Para su desarrollo se hizo consumo de una Api Rest de Pokémon, JavaScript y Bootstrap.",
@@ -45,10 +37,25 @@ export let projects: project[] = [
         code: "https://github.com/gisellem22/SCL009-data-lovers"
     },
     {
-        name: "BURGUER QUEEN",
-        text: "Plataforma web SPA diseñada para Tablet, que permite a los meseros de una cadena de restaurantes tomar los pedidos de los clientes. Implementado con Angular y Bootstrap como frameworks Front-End. FireStore como base de datos RealTime para almacenar los pedidos realizados.",
-        img: "../../../assets/img/projects/burger-queen.JPG",
-        demo: "https://burger-queen-dg.firebaseapp.com",
-        code: "https://github.com/gisellem22/SCL009-Burger-Queen"
+        name: "WOOD HOTEL-GROUP",
+        text: "Interfaz web que ayuda a un cliente en línea a encontrar el hotel más barato. Implementada con Angular, Firebase RealTime Firestore y Bootstrap.",
+        img: "../../../assets/img/projects/hotel.JPG",
+        demo: "https://desafio-hotel.firebaseapp.com",
+        code: "https://github.com/gisellem22/Deseafio-Hotel"
+    },
+    {
+        name: "GIGIBOOK",
+        text: "Interfaz web que representa la sección de comentarios de una plataforma. Implementada con Angular, Firebase RealTime Firestore y Bootstrap.",
+        img: "../../../assets/img/projects/comentarios.JPG",
+        demo: "https://desafio-comentarios.firebaseapp.com",
+        code: "https://github.com/gisellem22/Desafio-Comentarios"
     }
+    // ,
+    // {
+    //     name: "BURGUER QUEEN",
+    //     text: "Plataforma web SPA diseñada para Tablet, que permite a los meseros de una cadena de restaurantes tomar los pedidos de los clientes. Implementado con Angular y Bootstrap como frameworks Front-End. FireStore como base de datos RealTime para almacenar los pedidos realizados.",
+    //     img: "../../../assets/img/projects/burger-queen.JPG",
+    //     demo: "https://burger-queen-dg.firebaseapp.com",
+    //     code: "https://github.com/gisellem22/SCL009-Burger-Queen"
+    // }
 ]
